2742d4e9bea78a1debe09be585a2616101b10002
kannadaflix <126308349+kannadaflix@users.noreply.github.com>
2023-08-13 15:27:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/11f1d4c0ed25ae907ce4d.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/36b348d3261fdccec4045.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
